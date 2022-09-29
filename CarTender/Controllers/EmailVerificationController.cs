1dd27650968b8d66c63786572c0ecd0356929115
Sarp İpekçi <sarpipekci@outlook.com>
2022-09-30 02:50:59 +0300

    UI is done!

---
+++ CarTender/Controllers/EmailVerificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarTender.Controllers
+{
+    public class EmailVerificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarTender/Controllers/EmailVerificationCorrectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarTender.Controllers
+{
+    public class EmailVerificationCorrectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
